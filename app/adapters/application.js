b47281853f09d0671a32a135a252116a201afd42
Magnus Kull <magnus.kull@ub.gu.se>
2016-11-19 23:40:47 +0100

    backend host in environment.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,9 @@
+import ENV from '../config/environment';
+
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
 
   namespace: 'v1',
-  host: 'https://immense-escarpment-95738.herokuapp.com'
-
+  host: ENV.host
 });
